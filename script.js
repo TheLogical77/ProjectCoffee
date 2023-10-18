fe79f517bba796916ac71278a06e170cc45b6e0a
Chandranshu Gupta <114946276+TheLogical77@users.noreply.github.com>
2023-10-18 16:53:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ togglebtn.addEventListener("click", () => {
     bool = true;
   }
 
-  document.body.classList.toggle("light-theme");
+  // document.body.classList.toggle("light-theme");
 
   togglebtn.firstElementChild.classList.toggle("fa-sun");
 });
